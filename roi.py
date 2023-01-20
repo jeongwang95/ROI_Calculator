733e6995fcd8e2ea9b6d2ea9e294575c42ce1fdd
Jeong Wang <jeong@jeongs-mbp.myfiosgateway.com>
2023-01-20 16:51:45 -0500

    added edit property function

--- roi.py
+++ roi.py
@@ -1,3 +1,5 @@
+import re
+
 class Property():
     def __init__(self, address, state, rent, coi):
         self.address = address
@@ -5,8 +7,35 @@ class Property():
         self.rent = rent
         self.coi = coi
 
+    def change_rent(self):
+        print("""
+*****************************************************************
+Rent Data:
+        """)
+        with open('grossrents.txt') as f:
+            data = f.read()
+
+        us_data = re.search('(USA)\)\s+(\$\d+)', data)
+        state_data = re.search(f'([A-z ]+)\s+\(({self.state})\)\s+(\$\d+)', data)
+        # show median gross rent for USA and the State of the property
+        if state_data:
+            print(f"The current median gross rent in the United States is {us_data.group(2)}.")
+            print(f"The current median gross rent in the State of {state_data.group(1)} is {state_data.group(3)}.")
+            print(f"The current rent of {self.address} in {self.state} is ${self.rent}.")
+            print('*****************************************************************')
+
+        new_rent = float(input("What is the new rent? $"))
+        old_rent = self.rent
+        self.rent = new_rent
+        print(f"The rent of {self.address} in {self.state} has been updated from ${old_rent} to ${new_rent}!")
+
+    def change_coi(self, new_coi):
+        self.coi = new_coi
+
     def __repr__(self):
         return f"\nProperty: {self.address}\nLocation: {self.state}\nRent: ${self.rent}\nCOI: ${self.coi}"
+
+    
 
 class ROI():
     def __init__(self):
@@ -49,7 +78,23 @@ Current Users:
     def add_property(self, username):
         # property #, street name, and state are used to check that no duplicate properties are added to current user's property listings
         addy = input("Enter the property number and street name (ex. 123 Main St):\n").title().strip()
-        state = input("Enter the State where the property resides in abbreviated format (ex. for Maryland, enter MD):\n").upper().strip()
+        
+        # grab all United States State abbreviations and store in a list so we can
+        # make sure user enters the correct state abbreviations
+        with open('grossrents.txt') as f:
+            data = f.readlines()
+        state_abbreviations = []
+        for line in data:
+            found = re.search('\(([A-Z]{2})\)', line)
+            if found:
+                state_abbreviations.append(found.group(1)) 
+
+        while True:
+            state = input("Enter the State where the property resides in abbreviated format (ex. for Maryland, enter MD):\n").upper().strip()
+            if len(state) == 2 and state in state_abbreviations:
+                break
+            else:
+                print("Invalid State abbreviation. Please try again.")
         
         # in order to check if the property already exits
         # check arrays are created to hold all of the address/state attributes from our Property class
@@ -102,6 +147,28 @@ Current Users:
         roi = int((net_income/p.coi)*100)
         return roi
 
+    def edit_property(self, username):
+        addy = input("Enter the property number and street name of the property you want edit:\n").title().strip()
+        state = input("Enter the State where the property resides in abbreviated format (ex. for Maryland, enter MD):\n").upper().strip()
+
+        # loop through the user's property list to find a match
+        for p in self.users[username]:
+            # if the property is found, user can either change the rent or the coi
+            if addy == p.address and state == p.state:
+                print(f"The property: {addy} in {state} has been found in your list of properties!\n")
+                choice = input("Enter 'Rent' to change the rent. Enter 'COI' to change the cost of investment.\n").lower().strip()
+                
+                if choice in ('rent', 'change rent'):
+                    p.change_rent()
+                elif choice in ('coi', 'change coi', 'cost of invesment', 'change cost of invesment'):
+                    new_coi = float(input("What is the new cost of investment (COI)? $"))
+                    p.change_coi(new_coi)
+                else:
+                    print("Invalid input. Returning to User Menu")
+                    break
+        else:
+            print(f"\nThe property: {addy} in {state} is not in your list of properties.")
+
 class Main():
     def print_main_menu():
         print("""
@@ -126,7 +193,7 @@ Main Menu
 [2] Remove Property
 [3] Edit Property
 [4] View Property(s) & View ROI of Each Property
-[6] Back to Main Menu
+[5] Back to Main Menu
         """)
     
     def run():
@@ -151,12 +218,10 @@ Main Menu
                         elif response == '2':
                             roi.del_property(username)
                         elif response == '3':
-                            pass
+                            roi.edit_property(username)
                         elif response == '4':
                             roi.view_properties(username)
                         elif response == '5':
-                            roi.cal_roi(username)
-                        elif response == '6':
                             print("Returning to Main Menu")
                             break
                         else:
@@ -173,4 +238,5 @@ Main Menu
 
 
 Main.run()
-        
+
+
